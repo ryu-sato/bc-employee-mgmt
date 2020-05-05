05b7db885c2bb31fd73846a06f428f4cf386c327
Ryu Sato <ryu@weseek.co.jp>
2020-05-05 15:58:26 +0900

    employee モデルに validation と enum を設定し、モデル作成フォームにて enum の値を選択できるように修正した

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+  enum gender: [:male, :female, :other]
+
+  validates :name, presence: true
+  validates :department, presence: true
+  validates :payment, presence: true
 end
