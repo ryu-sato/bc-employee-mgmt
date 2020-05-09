1ca98aa2e5c13de64dd1d1c4370d49ee75ce59b7
Ryu Sato <ryu@weseek.co.jp>
2020-05-10 01:51:38 +0900

    i18n対応と勤務年数の表示

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,14 @@
 class Employee < ApplicationRecord
-  enum gender: [:male, :female, :other]
+  enum gender: { other: 0, male: 1, female: 2 }
 
   validates :name, presence: true
   validates :department, presence: true
   validates :payment, presence: true
+
+  def years_worked
+    today = Date.today()
+    diff_months = (today.year * 12 + today.month) - (joined_date.year * 12 + joined_date.month)
+    year, month = diff_months.divmod(12)
+    1 + year
+  end
 end
