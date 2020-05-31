16916d905fc751bf398601106c5e7a305536beed
Ryu Sato <ryu@weseek.co.jp>
2020-06-01 00:00:45 +0900

    インデントサイズを2に統一

--- spec/services/employee_matching_service_spec.rb
+++ spec/services/employee_matching_service_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe EmployeeMatchingService, type: :service do
-    describe '#compatibility' do
-        context 'when valid employees specified' do
-            let(:emp_a) { create :employee, name: '日本 太郎' }
-            let(:emp_b) { create :employee, name: '日本 次郎' }
+  describe '#compatibility' do
+    context 'when valid employees specified' do
+      let(:emp_a) { create :employee, name: '日本 太郎' }
+      let(:emp_b) { create :employee, name: '日本 次郎' }
 
-            it 'return valid compatibility' do
-                compatibility = EmployeeMatchingService.instance.compatibility(emp_a, emp_b)
-                expect(compatibility).to be >= 0
-                expect(compatibility).to be <= 100
-            end
-        end
-
-        context 'when invalid employees specified' do
-            it 'return invalid compatibility' do
-                compatibility = EmployeeMatchingService.instance.compatibility(nil, nil)
-                expect(compatibility).to be_nil
-            end
-        end
+      it 'return valid compatibility' do
+        compatibility = EmployeeMatchingService.instance.compatibility(emp_a, emp_b)
+        expect(compatibility).to be >= 0
+        expect(compatibility).to be <= 100
+      end
     end
+
+    context 'when invalid employees specified' do
+      it 'return invalid compatibility' do
+        compatibility = EmployeeMatchingService.instance.compatibility(nil, nil)
+        expect(compatibility).to be_nil
+      end
+    end
+  end
 end
